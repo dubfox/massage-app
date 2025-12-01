a8a4ce6910d85e40d8169fb634413a86046353f6
Christopher Ellis <christopherellis@Christophers-MacBook-Pro.local>
2025-12-01 17:25:21 +0700

    Update project

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -2,8 +2,8 @@
 
 # Build and run the Nantika massage app container on a specified host port.
 # Usage:
-#   ./docker-build-run.sh             # uses default host port 3000
-#   ./docker-build-run.sh 8080        # maps host 8080 -> container 3000
+#   ./docker-build-run.sh             # uses default host port 3000 (detached)
+#   ./docker-build-run.sh 8080        # maps host 8080 -> container 3000 (detached)
 
 set -e
 
@@ -21,6 +21,10 @@ echo
 echo "Running container from image '${IMAGE_NAME}' on host port ${HOST_PORT}..."
 echo "(host:${HOST_PORT} -> container:${CONTAINER_PORT})"
 
-docker run --rm -p "${HOST_PORT}:${CONTAINER_PORT}" "${IMAGE_NAME}"
+docker run -d --rm -p "${HOST_PORT}:${CONTAINER_PORT}" --name "${IMAGE_NAME}" "${IMAGE_NAME}"
+
+echo
+echo "Container '${IMAGE_NAME}' is running in the background."
+echo "Stop it with: docker stop ${IMAGE_NAME}"
 
 
